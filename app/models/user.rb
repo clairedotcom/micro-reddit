ae89430343ba84b059dd37798cebe6dc53233f67
clairedotcom <claireacarson@gmail.com>
2022-01-14 13:01:29 -0500

    Add Post model and user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true
+  validates :password, uniqueness: true, presence: true
 end
